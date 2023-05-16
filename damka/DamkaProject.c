f2646a2d7defdad4e3d498f5a0c5ee4556aaef09
fireflew11 <tombraudo2808@gmail.com>
2023-05-16 16:42:57 +0300

    instructions in main

--- damka/DamkaProject.c
+++ damka/DamkaProject.c
@@ -8,8 +8,29 @@
 typedef unsigned char Player;
 
 //can assume not NULL, pos = 0,1,...,7
+//this is main instructions are here
+//git instructions:
+/*
 
+after finishing a function or a session, 
+save and close visual studios
+then open CMD
+then apply these commands:
+cd "project-path"
+git add .
+git commit -m "name-of-the-update" (give meaningfull name, what changed)
+git push
 
+and then it will be on the site
+
+*/
+//working together instructions:
+/*
+on every function or we we write, comment so the other could understand what was written, even without asking directly
+never commit push something that didn't work in testing/debugging
+have fun :)
+
+*/
 
 
 void main() {
